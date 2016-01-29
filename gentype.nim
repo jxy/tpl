a4996e5f78eaef1e61e90ff095e8f7aa86fc77c6
Xiao-Yong Jin <xjin@anl.gov>
2016-01-29 12:22:42 -0600

    reformat and dummy types

--- gentype.nim
+++ gentype.nim
@@ -22,10 +22,12 @@ macro unrollfor(i: untyped, lo, hi: int, n: untyped): stmt =
     result.add(n.replace(i, newIntLitNode(j)))
 
 ####################
-# index types
+# index type
 type
-  gTindex[id,lo,hi:static[int]] = object
-    i: int
+  gTindex[id,lo,hi:static[int]] = object {.requiresInit.}
+    # `i` auto inits to 0, which is bad
+    # `requiresInit` in v0.13 gives warning without an explicit initialization
+    i: range[lo..hi]
 proc `$`(x: gTindex): string =
   "indexType[" &
     $gTindex.id & "," &
@@ -33,23 +35,23 @@ proc `$`(x: gTindex): string =
     $gTindex.hi &
     "] = " & $x.i
 var IndexID {.compileTime.} = 0
-macro Index(lo, hi: int): expr =
-  echo "\n>>>> Index(lo,hi)"
+macro index(lo, hi: int): expr =
+  echo "\n>>>> index(lo,hi)"
   result = newNimNode(nnkBracketExpr).add(
     bindSym"gTindex", IndexID.newIntLitNode, lo, hi)
   inc IndexID
   debug result
-  echo "<<<< Index(lo,hi)"
+  echo "<<<< index(lo,hi)"
 template staticInbound(n, lo, hi: int) =
   static:
     if n < lo or n > hi:
       error "index out of bounds: " & $n
-template Index(t:typedesc, n:int): expr =
+template index(t:typedesc, n:int): expr =
   n.staticInbound t.lo, t.hi
   t(i: n)
-template Index(n:int, t:typedesc): expr =
-  Index(t, n)
-proc `Index=`(ix:var gTindex, n:static[int]) {.inline.} =
+template index(n:int, t:typedesc): expr =
+  index(t, n)
+proc `index=`(ix:var gTindex, n:static[int]) {.inline.} =
   n.staticInbound gTindex.lo, gTindex.hi
   ix.i = n
 
@@ -88,40 +90,68 @@ template `[]=`[V;id1,l1,id2,l2,lo1,hi1,lo2,hi2:static[int]](x: var gT2[V,id1,l1,
   let i = (i2.i-lo2)*l1+i1.i-lo1
   x.data[i] = y
 
+####################
+# dummy index type
+type
+  gTindexDummy[id,lo,hi:static[int]] = object
+template dummy[id,lo,hi:static[int]](t: typedesc[gTindex[id,lo,hi]]): expr =
+  gTindexDummy[id,lo,hi]
+proc `[]`[V;id1,l1,lo1,hi1:static[int]](x: gT1[V,id1,l1], i1: gTindexDummy[id1,lo1,hi1]): V = discard
+proc `[]`[V;id1,l1,id2,l2,lo1,hi1,lo2,hi2:static[int]](x: gT2[V,id1,l1,id2,l2], i1: gTindexDummy[id1,lo1,hi1], i2: gTindexDummy[id2,lo2,hi2]): V = discard
+proc `[]=`[V;id1,l1,lo1,hi1:static[int]](x: var gT1[V,id1,l1], i1: gTindexDummy[id1,lo1,hi1], y: V) = discard
+proc `[]=`[V;id1,l1,id2,l2,lo1,hi1,lo2,hi2:static[int]](x: var gT2[V,id1,l1,id2,l2], i1: gTindexDummy[id1,lo1,hi1], i2: gTindexDummy[id2,lo2,hi2], y: V) = discard
+
+####################
+# tensor ops
+macro tensorOps(n: typed): typed =
+  echo "\n>>>> tensorOps"
+  echo n.treerepr
+  echo "<<<< tensorOps"
 
 when isMainModule:
   type
-    Spin = Index(1,4)
-    Color = Index(1,4)
-  assert(not(Spin is Color), "Spin shouldn't be the same as Color")
-  var
-    s: Spin
-    # ss = 5.Index(Spin)            # compile time error: out of bounds
-    c = Color.Index 2
-    # c2 = Index(0,Color)           # compile time error: out of bounds
-  echo c
-  c.Index = 1
-  echo c
-  echo s
-  # s = Color.Index(3)              # compile time error: wrong type
-  const
-    one = 1
-    two = 2
-  s = Spin.Index(two * one)
-  echo s
+    Spin = index(1,4)
+    Color = index(1,4)
+  block:
+    assert(not(Spin is Color), "Spin shouldn't be the same as Color")
+    var
+      s: Spin
+      # ss = 5.index(Spin)            # compile time error: out of bounds
+      c = Color.index 2
+      # c2 = index(0,Color)           # compile time error: out of bounds
+    echo c
+    c.index = 1
+    echo c
+    echo s, "  initialized to 0, which is bad, how can we check?"
+    # s = Color.index(3)              # compile time error: wrong type
+    const
+      one = 1
+      two = 2
+    s = Spin.index(two * one)
+    echo s
 
-  var
-    sv: Tensor(float, Spin)
-  echo 1, " : ", sv[Spin.Index 1]
-  echo 2, " : ", sv[Spin.Index 2]
-  echo 3, " : ", sv[Spin.Index 3]
-  echo 4, " : ", sv[Spin.Index 4]
-  type
-    s2 = Index(3, 4)
-    c3 = Index(0, 2)
-  var
-    scv: Tensor(float, s2, c3)
-  unrollfor j, 0, 2:
-    unrollfor i, 3, 4:
-      scv[s2.Index i, c3.Index j] = float i*10+j
-      echo "[", i, ",", j, "] : ", scv[s2.Index i, c3.Index j]
+  block:
+    var
+      sv: Tensor(float, Spin)
+    unrollfor i, 1, 4:
+      echo "[", i, "]: ", sv[Spin.index i]
+
+  block:
+    type
+      s2 = index(3, 4)
+      c3 = index(0, 2)
+    var
+      scv: Tensor(float, s2, c3)
+    unrollfor j, 0, 2:
+      unrollfor i, 3, 4:
+        scv[s2.index i, c3.index j] = float i*10+j
+        echo "[", i, ",", j, "]: ", scv[s2.index i, c3.index j]
+
+  block:
+    var
+      a, b: dummy(Spin)
+    tensorOps:
+      var
+        x, y: Tensor(float, Spin)
+        m: Tensor(float, Spin, Spin)
+      y[a] = m[a,b] * x[b] + x[a]
