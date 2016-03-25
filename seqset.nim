b4498aea3a0b4637d62f01cbab9a1fe8d3f3f37c
Xiao-Yong Jin <xjin@anl.gov>
2016-03-25 15:31:08 -0500

    fix seqset for `-` and `+`
    Properly create a new seqset

--- seqset.nim
+++ seqset.nim
@@ -8,6 +8,10 @@ iterator items*[T](s: seqset[T]): T =
 iterator pairs*[T](s: seqset[T]): (int, T) =
   for i, j in s.s:
     yield (i, j)
+proc copy*[T](s: seqset[T]): seqset[T] =
+  result.s.newseq 0
+  for x in s:
+    result.s.add x
 proc contains*[T](s: seqset[T], x: T): bool =
   for i in s:
     if x == i:
@@ -20,29 +24,29 @@ proc contains*[T](s, xs: seqset[T]): bool =
   return true
 proc init*[T](s: var seqset[T]) = newseq(s.s,0)
 proc incl*[T](s: var seqset[T], x: T) =
-  if not (x in s):
-    s.s.add(x)
+  if x notin s:
+    s.s.add x
 proc incl*[T](s: var seqset[T], x: seqset[T]) =
   for i in x:
-    s.incl(i)
+    s.incl i
 proc `+`*[T](x: seqset[T], y: seqset[T]): seqset[T] =
-  result = x
-  result.incl(y)
+  result = x.copy
+  result.incl y
 proc `+=`*[T](x: var seqset[T], y: seqset[T]) =
-  x.incl(y)
+  x.incl y
 proc excl*[T](s: var seqset[T], x: T) =
   for n, i in s.s:
     if x == i:
-      s.s.del(n)
+      s.s.del n
       break
 proc excl*[T](s: var seqset[T], x: seqset[T]) =
   for i in x:
     s.excl i
 proc `-`*[T](x: seqset[T], y: seqset[T]): seqset[T] =
-  result = x
-  result.excl(y)
+  result = x.copy
+  result.excl y
 proc intersection*[T](x: seqset[T], y:seqset[T]): seqset[T] =
   result.init
   for i in x:
     if i in y:
-      result.incl(i)
+      result.incl i
