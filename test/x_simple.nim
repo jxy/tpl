5848520b76123b715ef08526c37670836c401998
Xiao-Yong Jin <xjin@anl.gov>
2016-08-30 13:49:54 -0500

    Add readme and an example

---
+++ test/x_simple.nim
@@ -0,0 +1,32 @@
+import TPL
+
+tpl:
+  S = IndexType(4)
+  C = IndexType(3)
+  type
+    SV = Tensor([S], float)
+    CV = Tensor([C], float)
+    CM = Tensor([C,C], float)
+    CSV = Tensor([C,S], float)
+    SCM = Tensor([S,C,C], float)
+  var
+    x: float
+    sv: SV
+    cv: CV
+    cm: CM
+    csv: CSV
+    scm: SCM
+    i = S.dummy
+    a = C.dummy
+  # sv+/ .*cv+/ .*"1 (scm=:(csv=:(sv=:1+i.4)*/cv)*/cv)+/ .*"2 1 cv=:0.1*1+i.3
+  sv[i] = 1.0+i
+  cv[a] = 0.1*(1.0+a)
+  csv = sv*cv
+  scm = csv*cv
+  x = sv*cv*scm*cv
+  cm = scm[S.index 2]
+  echo "scm[",2,"] =\n",cm
+  echo "sv = ",sv
+  echo "cv = ",cv
+  echo "csv = ",csv
+  echo "x = sv*cv*scm*cv = ", x
